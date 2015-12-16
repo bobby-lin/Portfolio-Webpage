d2b8f4b62a2b10448d73fd23efe1041b0a77fe44
Bobby Lin <bobbylinjh@gmail.com>
2015-12-16 15:59:52 +0800

    Added closing of autoscroll and offset in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -2,3 +2,13 @@
  * Created on: 16/12/15
  *     Author: Bobby Lin
  */
+
+$('.nav a').on('click', function(){
+    $(".navbar-toggle").click();
+    var adjustment = 50;
+    var speed = 1;
+    var header = this.hash;
+    $('html,body').animate({
+        scrollTop: $(header).offset().top - adjustment
+    }, speed);
+});
