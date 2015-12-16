5d84ae766f0a8e5fe4b4b2272d3acc91f28bc240
Bobby Lin <bobbylinjh@gmail.com>
2015-12-16 16:48:08 +0800

    Added toggle when visible for navbar in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -4,11 +4,14 @@
  */
 
 $('.nav a').on('click', function(){
-    $(".navbar-toggle").click();
+    if (navbar_toggle.is(':visible')) {
+        $(".navbar-toggle").click();
+    }
     var adjustment = 50;
     var speed = 1;
     var header = this.hash;
     $('html,body').animate({
         scrollTop: $(header).offset().top - adjustment
     }, speed);
+    
 });
